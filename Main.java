85199a3540fec40151d0a4c79ab6fe7ce8328e8e
younj1601 <younj1601@wrdsb.ca>
2020-10-01 15:36:06 +0000

    Finished the third question for 2.01

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will tell the user the mark range for a specific grade
+ * @author Jackson 
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  //create a scanner for user input
+  Scanner input = new Scanner(System.in);
+
+  //get the grade from the user
+  System.out.println("Please enter a letter grade");
+  String grade = input.nextLine();
+
+  //determine the percentages
+  switch(grade){
+    case "A":
+    System.out.println("This is between 80% and 100%");
+    break;
+    case "B":
+    System.out.println("This is between 70% and 79%");
+    break;
+    case "C":
+    System.out.println("This is between 60% and 69%");
+    break;
+    case "D":
+    System.out.println("This is between 50% and 59%");
+    break;
+    case "F":
+    System.out.println("This is between 0% and 49%");
+    break;
+  }
     
   }
 }
